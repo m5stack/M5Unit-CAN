46c1f7a5012a13b4affba40b8ccfc2226b0744b3
sean <1026025056@qq.com>
2023-11-30 10:12:45 +0800

    init

---
+++ examples/CANReceiverCallback/CANReceiverCallback.ino
@@ -0,0 +1,80 @@
+/**
+ * @file CANReceiverCallback.ino
+ * @author SeanKwok (shaoxiang@m5stack.com)
+ * @brief M5Unit CAN Receiver Callback Example
+ * @version 0.1
+ * @date 2023-11-30
+ * 
+ * 
+ * @Hardwares: M5Unit CAN
+ * @Platform Version: Arduino M5Stack Board Manager v2.0.7
+ * @Dependent Library:
+ * arduino-CAN: https://github.com/sandeepmistry/arduino-CAN
+ * M5Unified: https://github.com/m5stack/M5Unified
+ */
+
+
+#include "M5Unified.h"
+#include <CAN.h>
+
+#define CAN_TX_PIN 26
+#define CAN_RX_PIN 36
+
+void onReceive(int packetSize);
+
+void setup() {
+    M5.begin();
+
+    Serial.println("CAN Sender");
+
+    CAN.setPins(CAN_RX_PIN, CAN_TX_PIN);
+
+    // start the CAN bus at 500 kbps
+    if (!CAN.begin(500E3)) {
+        Serial.println("Starting CAN failed!");
+        while (1)
+            ;                   
+    }
+
+  // register the receive callback
+  CAN.onReceive(onReceive);
+}
+
+void loop() {
+  delay(100);
+}
+
+void onReceive(int packetSize) {
+  // received a packet
+  Serial.print("Received ");
+
+  if (CAN.packetExtended()) {
+    Serial.print("extended ");
+  }
+
+  if (CAN.packetRtr()) {
+    // Remote transmission request, packet contains no data
+    Serial.print("RTR ");
+  }
+
+  Serial.print("packet with id 0x");
+  Serial.print(CAN.packetId(), HEX);
+
+  if (CAN.packetRtr()) {
+    Serial.print(" and requested length ");
+    Serial.println(CAN.packetDlc());
+  } else {
+    Serial.print(" and length ");
+    Serial.println(packetSize);
+
+    // only print packet data for non-RTR packets
+    while (CAN.available()) {
+      Serial.print((char)CAN.read());
+    }
+    Serial.println();
+  }
+
+  Serial.println();
+}
+
+
---
+++ src/M5_CAN.h
@@ -0,0 +1,8 @@
+#ifndef _M5_UNIT_CAN_H
+#define _M5_UNIT_CAN_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <CAN.h>
+
+#endif
